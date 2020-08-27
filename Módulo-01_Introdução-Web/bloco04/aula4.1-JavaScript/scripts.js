9947854ae935d54cf8ac1456c28e50a8c683dc2a
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-08-27 17:45:19 -0300

    Adicionando pasta Dia 4.1 -27/08

---
+++ Módulo-01_Introdução-Web/bloco04/aula4.1-JavaScript/scripts.js
@@ -0,0 +1,14 @@
+// var é o modelo antigo de definir uma variável;
+// let é o modelo mais atual hj em dia;
+
+let nome = "André Horman" // STRING - PALAVRAS
+console.log (nome);
+
+let idade = 25; // NUMBER - NÚMEROS
+console.log (idade);
+
+let aprovado = true; // BOOLEAN - CONDICIONAL
+
+let vazio = undefined // UNDEFINED  - VAR SEM VALOR
+
+let preencha = null // NULL - REDEFINIR UM VALOR
