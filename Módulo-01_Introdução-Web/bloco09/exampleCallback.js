29d71b859b643673cd5724aa6933f085b160756e
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-10-21 18:40:40 -0300

    Atualizando Pastas - 20/10/2020

---
+++ Módulo-01_Introdução-Web/bloco09/exampleCallback.js
@@ -0,0 +1,29 @@
+const main = callback => {
+  const message = "Executando meu callback"
+  callback(message);
+}
+
+main((string) => {
+  console.log(string);
+});
+
+// EXECUTANDO UMA FUNÇÃO ASSÍNCRONA UTILIZANDO CALLBACKS
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = () => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian"
+    };
+    console.log(user);
+  }, delay());
+};
+
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
