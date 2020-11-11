5a02668177256332622f04bc77f816b8d5cec707
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-11 20:25:47 -0300

    Criando um formulario e states - 11/11/2020

--- Módulo-02_Front-End/bloco12/aula12.2-forms-react/src/App.jsx
+++ Módulo-02_Front-End/bloco12/aula12.2-forms-react/src/App.jsx
@@ -1,11 +1,13 @@
+import Forms01 from './components/ExForms01';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      oi
-    </div>
-  );
+	return (
+		<div className='App'>
+      <h1>FORMULÁRIO REACT</h1>
+			<Forms01 />
+		</div>
+	);
 }
 
 export default App;
