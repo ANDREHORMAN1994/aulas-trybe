d8fe25e3c1d3aa30d82a2a15e94f97253898c98b
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-14 18:34:26 -0300

    Criando Botao de cliques - 14/11/2020

--- Módulo-02_Front-End/bloco12/aula12.1-eventos-estados/src/App.js
+++ Módulo-02_Front-End/bloco12/aula12.1-eventos-estados/src/App.js
@@ -1,14 +1,17 @@
+import React from 'react';
 import './App.css';
 import Button from './components/button';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Aula 12.1</h1>
-      <h2>Eventos e Estados React</h2>
-      <Button />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Aula 12.1</h1>
+        <h2>Eventos e Estados React</h2>
+        <Button />
+      </div>
+    );
+  }
 }
 
 export default App;
