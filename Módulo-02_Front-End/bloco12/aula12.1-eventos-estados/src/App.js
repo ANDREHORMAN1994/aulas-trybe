88d496f83914d154f28566937b0af0d61430da1a
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-14 13:57:54 -0300

    Testando um package para o prettier e eslint, imaginary-cloud - 14/11/2020

--- Módulo-02_Front-End/bloco12/aula12.1-eventos-estados/src/App.js
+++ Módulo-02_Front-End/bloco12/aula12.1-eventos-estados/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
