92279cc52a6867e85cbc11563e5006f9c37aab0b
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-14 16:27:24 -0300

    Iniciando Exercicio de fixaçao do 12.1 - 14/11/2020

--- Módulo-02_Front-End/bloco12/aula12.1-eventos-estados/src/App.js
+++ Módulo-02_Front-End/bloco12/aula12.1-eventos-estados/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import './App.css'
+import './App.css';
+import Button from './components/button';
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO WORLD</h1>
+      <h1>Aula 12.1</h1>
+      <h2>Eventos e Estados React</h2>
+      <Button />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Módulo-02_Front-End/bloco12/aula12.1-eventos-estados/src/components/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>Clique aqui: 0</button>
+      </div>
+    );
+  }
+}
+
+export default Button;
