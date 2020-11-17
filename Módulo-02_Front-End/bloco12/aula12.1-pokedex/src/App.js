e9c2d78c9ce1ea8a0c4179a39f04ead5c1cf0d71
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-17 14:28:28 -0300

    Implementando logica da Pokedex - 14/11/2020

--- Módulo-02_Front-End/bloco12/aula12.1-pokedex/src/App.js
+++ Módulo-02_Front-End/bloco12/aula12.1-pokedex/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Pokedex from './components/pokedex';
+import Data from './data';
 
 function App() {
   return (
     <div className="App">
       <h1>POKEDEX</h1>
       <h2>Exercício 12.1</h2>
+      <Pokedex pokemonList={Data} />
     </div>
   );
 }
--- Módulo-02_Front-End/bloco12/aula12.1-pokedex/src/components/pokedex.jsx
+++ Módulo-02_Front-End/bloco12/aula12.1-pokedex/src/components/pokedex.jsx
@@ -1,5 +1,72 @@
 import React from 'react';
+import Pokemon from './pokemon';
 
 class Pokedex extends React.Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.nextPokemon = this.nextPokemon.bind(this);
+    this.nextCount = this.nextCount.bind(this);
+    this.resetCount = this.resetCount.bind(this);
+
+    const { pokemonList } = this.props;
+    this.state = {
+      count: 0,
+      pokemon: pokemonList[0].id,
+    };
+  }
+
+  nextPokemon() {
+    const { pokemonList } = this.props;
+
+    this.setState({
+      pokemon: pokemonList[this.state.count].id,
+    });
+  }
+
+  resetCount() {
+    const { pokemonList } = this.props;
+
+    if (this.state.count === pokemonList.length - 1) {
+      this.setState({
+        count: 0,
+      });
+    }
+  }
+
+  nextCount() {
+    const { pokemonList } = this.props;
+
+    this.setState(
+      (previousElement) => ({
+        count: previousElement.count + 1,
+      }),
+      () => {
+        this.nextPokemon();
+        this.resetCount();
+        console.log(this.state.count);
+        console.log(pokemonList[this.state.count].id);
+      },
+    );
+  }
+
+  render() {
+    const { pokemonList } = this.props;
+    return (
+      <div>
+        <div className="pokemon-list">
+          {pokemonList.map((pokemon) => {
+            let result;
+            if (this.state.pokemon === pokemon.id) {
+              result = <Pokemon key={pokemon.id} pokemon={pokemon} />;
+            }
+            return result;
+          })}
+        </div>
+        <button onClick={this.nextCount}>Próximo Pokemon</button>
+      </div>
+    );
+  }
 }
+
+export default Pokedex;
