dd3a8e4155695d85464dbca25dc1bbd00b0346eb
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-03-25 12:55:43 -0300

    criando fibonacci - 25/03/2021

---
+++ Módulo-03_Back-End/bloco26/aula26.1-Intro-NodeJs/functions/fibonacci.js
@@ -0,0 +1,40 @@
+const inquirer = require('inquirer');
+const question = inquirer.prompt([
+  {
+    type: 'string',
+    name: 'fibonacci',
+    message: 'Escolha o valor de n:',
+    default: 0,
+    validate: (value) => {
+      if (!value.includes('.') && !value.includes(',') && parseInt(value, 10)) {
+        return true;
+      }
+      return 'formato inválido';
+    },
+  },
+]);
+
+function Fibonacci(num){
+	var before = 0;
+	var actual = 1;
+	var next;
+
+	for(let i = 0; i < num; i++){
+		if (i < 2) {
+      console.log(i);
+    } else {
+      next = before + actual;
+      before = actual;
+      actual = next;
+      console.log(next);
+    }
+	}
+}
+// console.log(Fibonacci(10));
+
+const calcularFibonacci = () => {
+  question.then((data) => {
+    console.log(Fibonacci(data.fibonacci));
+  })
+}
+calcularFibonacci();
